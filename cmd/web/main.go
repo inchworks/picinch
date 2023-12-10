ec741d2389c72762fc685c2685f8f9573f64a6cd
inchworks <53937883+inchworks@users.noreply.github.com>
2023-12-10 22:21:02 +0000

    Change version.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -53,7 +53,7 @@ import (
 
 // version and copyright
 const (
-	version = "1.0.16"
+	version = "1.0.17"
 	notice  = `
 	Copyright (C) Rob Burke inchworks.com, 2020.
 	This website software comes with ABSOLUTELY NO WARRANTY.
