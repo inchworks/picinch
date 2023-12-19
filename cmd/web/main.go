55cede1e5a8c8d989a3bf12ab05e2a09a86c8813
inchworks <53937883+inchworks@users.noreply.github.com>
2023-12-19 17:04:50 +0000

    Upgrade dependencies, for CVE-2023-48795.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -53,7 +53,7 @@ import (
 
 // version and copyright
 const (
-	version = "1.0.18"
+	version = "1.0.19"
 	notice  = `
 	Copyright (C) Rob Burke inchworks.com, 2020.
 	This website software comes with ABSOLUTELY NO WARRANTY.
